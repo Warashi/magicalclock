e37a3fa1b312ca32173f9cf6b5db8159e37ecadf
Shinnosuke Sawada <sawada@nlp.ist.i.kyoto-u.ac.jp>
2018-01-05 21:05:23 +0900

    add comments

--- clock.go
+++ clock.go
@@ -1,13 +1,16 @@
+// Package magicalclock は一日が何時間かを指定して、それに併せて1秒の長さを調整するパッケージ
 package magicalclock
 
 type clock int
 
+// Clock は時刻を改変するApplyを持つinterface
 type Clock interface {
 	Apply(hour, minute, second int) (h, m, s int)
 }
 
 func New(length int) Clock {
 	return clock(length)
+// New の引数には一日を何時間に設定するかを指定
 }
 
 func (c clock) Apply(h, m, s int) (hour, minute, second int) {
